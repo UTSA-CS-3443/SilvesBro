f7dabbda3e67f7b3bbd6dbebf6375037b8e8e68f
ceedott <146248299+ceedott@users.noreply.github.com>
2025-04-16 12:40:06 -0500

    made the slider bar work

--- app/src/main/java/edu/utsa/cs3443/silvesbro/TimerActivity.java
+++ app/src/main/java/edu/utsa/cs3443/silvesbro/TimerActivity.java
@@ -1,6 +1,8 @@
 package edu.utsa.cs3443.silvesbro;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ public class TimerActivity extends AppCompatActivity {
 
     TextView timerSliderVal;
     Slider timerSlider;
+    Button startTimerButton;
 
 
     @Override
@@ -24,7 +27,17 @@ public class TimerActivity extends AppCompatActivity {
         timerSlider.addOnChangeListener(new Slider.OnChangeListener() {
             @Override
             public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
-                
+                value = Math.round(value);
+                int intVal = (int)value; // pass this value to create the timer
+                timerSliderVal.setText(Integer.toString(intVal));
+            }
+        });
+
+        startTimerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+                //set timer and switch screen to main
             }
         });
 
