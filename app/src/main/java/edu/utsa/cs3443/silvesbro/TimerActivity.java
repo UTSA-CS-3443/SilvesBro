f074b3d400c694dc2634acd59181fc7daed38445
ceedott <146248299+ceedott@users.noreply.github.com>
2025-04-16 11:54:19 -0500

    updated timer classes

--- app/src/main/java/edu/utsa/cs3443/silvesbro/TimerActivity.java
+++ app/src/main/java/edu/utsa/cs3443/silvesbro/TimerActivity.java
@@ -1,12 +1,32 @@
 package edu.utsa.cs3443.silvesbro;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.slider.Slider;
+
 public class TimerActivity extends AppCompatActivity {
+
+    TextView timerSliderVal;
+    Slider timerSlider;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer); // make sure this matches your XML file
+        timerSliderVal = findViewById(R.id.timer_slider_val);
+        timerSlider = findViewById(R.id.timer_slider);
+
+        timerSlider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                
+            }
+        });
+
     }
 }
