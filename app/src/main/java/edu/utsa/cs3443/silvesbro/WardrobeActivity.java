867e55db2f61a172dd9dc6538b9e75d47d65fdd3
ceedott <146248299+ceedott@users.noreply.github.com>
2025-04-17 15:20:01 -0500

    started WardrobeActivity

--- app/src/main/java/edu/utsa/cs3443/silvesbro/WardrobeActivity.java
+++ app/src/main/java/edu/utsa/cs3443/silvesbro/WardrobeActivity.java
@@ -3,10 +3,30 @@ package edu.utsa.cs3443.silvesbro;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class WardrobeActivity extends AppCompatActivity {
+
+    private ArrayList<WardrobeItem> availableItems;
+    WardrobeItem currentItem;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wardrobe);
     }
+
+    public void loadWardrobeItems() {
+        //read from csv
+    }
+
+    public void equipItem(String itemID){
+        //search from wardrobe items
+    }
+
+    public ArrayList<WardrobeItem> getAvailableItems () {
+        return this.availableItems;
+    }
+
 }
